509e87075dcd40f5df4e6e2fa8194802089fc0d2
realhautegirl <115960944+sgcxliv@users.noreply.github.com>
2025-04-08 15:23:44 -0700

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -57,7 +57,6 @@ export default function Layout({ children, title = 'REGLIST18N: What is Called L
       <Head>
         <title>{title}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <div id="main-content">
         <header className="course-header">
